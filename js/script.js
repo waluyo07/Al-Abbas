48dfe0e4577d63412618d49c6c4b097e0b2c9246
waluyo07 <waluyowae7@gmail.com>
2023-06-16 00:14:01 +0700

    alabbas

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+      // dots: true,
+      autoplay: true,
+      autoplaySpeed: 2500,
+      
+      // slidesToShow: 1,
+      // slidesToScroll: 1,
+    });
+  });
